c8ea7cbf16e4db23c5a81e72f5d39329fcdd8d6e
Leon8982 <144517329+Leon8982@users.noreply.github.com>
2023-09-13 17:48:34 +0530

    Update config.js

--- config.js
+++ config.js
@@ -14,7 +14,7 @@ function convertToBool(text, fault = 'true') {
 
 // PUT YOUR SESSION ID HERE  ##Required 
 
-global.SESSION_ID = '' // PUT your session ID BY scan QR 
+global.SESSION_ID = 'QueenNilu;;;F2I2TTpJ#7K3oy5DsQUfZH0avnGOs6jnu5K4qbaHKX-dInQmyksg' // PUT your session ID BY scan QR 
 
 // databse URL  ##Required 
 global.POSTGRESQL_URL = ''
